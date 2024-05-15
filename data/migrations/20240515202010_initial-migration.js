0ee4ff7e5dc0a830dbf94a3b5b2a3ea919a10121
Marijana Taseska <marijana.taseska@gmail.com>
2024-05-15 14:06:14 -0700

    project_resources table finished

--- data/migrations/20240515202010_initial-migration.js
+++ data/migrations/20240515202010_initial-migration.js
@@ -29,6 +29,25 @@ exports.up = async function (knex) {
             .onDelete('RESTRICT')
             .onUpdate('RESTRICT')
         })
+        .createTable('project_resources', tbl => {
+            tbl.increments('project_resource_id')
+            
+            tbl.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('project_id')
+            .inTable('projects')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
+
+            tbl.integer('resource_id')
+            .unsigned()
+            .notNullable()
+            .references('resource_id')
+            .inTable('resources')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
+        })
 };
 
 /**
