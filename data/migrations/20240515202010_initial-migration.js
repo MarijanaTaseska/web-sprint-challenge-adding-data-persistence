967166db9187b43dcc645a6301099d650745629d
Marijana Taseska <marijana.taseska@gmail.com>
2024-05-15 13:52:21 -0700

    tables completed

--- data/migrations/20240515202010_initial-migration.js
+++ data/migrations/20240515202010_initial-migration.js
@@ -5,13 +5,29 @@
 exports.up = async function (knex) {
     await knex.schema
         .createTable('projects', tbl => {
-            tbl.increments()
+            tbl.increments('project_id')
+            tbl.string('project_name', 200).notNullable()
+            tbl.text('project_description')
+            tbl.boolean('project_completed').defaultTo(false)
+
         })
         .createTable('resources', tbl => {
-            tbl.increments()
+            tbl.increments('resource_id')
+            tbl.string('resource_name').notNullable().unique()
+            tbl.text('resource_description')
         })
         .createTable('tasks', tbl => {
-            tbl.increments()
+            tbl.increments('task_id')
+            tbl.text('task_description').notNullable()
+            tbl.text('task_notes')
+            tbl.boolean('task_completed').defaultTo(false)
+            tbl.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('project_id')
+            .inTable('projects')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
         })
 };
 
