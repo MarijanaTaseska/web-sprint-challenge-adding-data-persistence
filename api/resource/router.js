34680333333d460441f0cacb02eccf2b0b48a536
Marijana Taseska <marijana.taseska@gmail.com>
2024-05-15 09:02:55 -0700

    index, server,router wired

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,17 @@
 // build your `/api/resources` router here
+const router = require('express').Router()
+
+
+router.use('/', (req, res)=>{
+    res.json({message:'hello from resources router'})
+})
+
+router.use((err, req, res, next) => { //eslint-disable-line
+    res.status(500).json({
+        customMessage:"Something went wrong inside the resources router ",
+        message:err.message,
+        stack:err.stack,
+    })
+})
+
+module.exports=router
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,17 @@
 // build your `/api/tasks` router here
+const router = require('express').Router()
+
+
+router.use('/', (req, res)=>{
+    res.json({message:'hello from tasks router'})
+})
+
+router.use((err, req, res, next) => { //eslint-disable-line
+    res.status(500).json({
+        customMessage:"Something went wrong inside the tasks router ",
+        message:err.message,
+        stack:err.stack,
+    })
+})
+
+module.exports=router
